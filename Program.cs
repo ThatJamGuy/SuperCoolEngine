008018c35020ed8223acb0b870450a31b549620f
ThatJamGuy <soniccatcraft@gmail.com>
2026-06-12 21:44:39 -0400

    Cool things

--- Program.cs
+++ Program.cs
@@ -6,11 +6,11 @@ internal static class Program {
     private const int windowWidth = 800;
     private const int windowHeight = 480;
 
-    private static AudioStream backgroundMusic;
-
     public static void Main() {
         Raylib.InitWindow(windowWidth, windowHeight, "Test Program");
         Raylib.InitAudioDevice();
+
+        Raylib.SetTargetFPS(120);
 
         Camera3D camera;
         camera.Position = new Vector3(10, 10, 10);
@@ -20,6 +20,11 @@ internal static class Program {
         camera.Projection = CameraProjection.Perspective;
 
         Vector3 cubePosition = new(0, 0, 0);
+
+        Model model = Raylib.LoadModel("resources\\models\\scp173.glb");
+        Texture2D texture = Raylib.LoadTexture("resources\\textures\\scp173.png");
+
+        Raylib.SetMaterialTexture(ref model, 0, MaterialMapIndex.Diffuse, ref texture);
 
         // Main loop
         while (!Raylib.WindowShouldClose()) {
@@ -42,17 +47,20 @@ internal static class Program {
             Raylib.DrawLine3D(cubePosition, new Vector3(cubePosition.X, cubePosition.Y + 2, cubePosition.Z), Color.Green);
             Raylib.DrawLine3D(cubePosition, new Vector3(cubePosition.X, cubePosition.Y, cubePosition.Z + 2), Color.Blue);
 
-            Raylib.DrawCube(cubePosition, 1, 1, 1, Color.Blue);
-            Raylib.DrawCubeWires(cubePosition, 1, 1, 1, Color.Black);
+            Raylib.DrawModel(model, cubePosition, 1, Color.White);
 
             Raylib.EndMode3D();
-            Raylib.EndDrawing();
 
-            Raylib.DrawText("Running the super cool rendering engine by ThatJamGuy", 10, 30, 10, Color.White);
+            Raylib.DrawText("Running the super cool rendering engine by ThatJamGuy", 10, 40, 15, Color.White);
             Raylib.DrawFPS(10, 10);
+
+            Raylib.EndDrawing();
         }
 
         // De-initialize all the things
+        Raylib.UnloadModel(model);
+        Raylib.UnloadTexture(texture);
+        Raylib.CloseAudioDevice();
         Raylib.CloseWindow();
     }
 }
